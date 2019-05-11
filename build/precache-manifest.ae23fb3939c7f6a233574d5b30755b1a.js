607011591354846cc491dc41b2e078ba7d160144
Liliana Listus <liliana-listus@pluralsight.com>
2019-05-11 13:33:12 +0300

    Add build.

---
+++ build/precache-manifest.ae23fb3939c7f6a233574d5b30755b1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a6c2d4d1181aaf556659",
+    "url": "/static/js/main.9aedc4e4.chunk.js"
+  },
+  {
+    "revision": "86d1853b74d0b5de8bb3",
+    "url": "/static/js/2.2bfcd6ae.chunk.js"
+  },
+  {
+    "revision": "a6c2d4d1181aaf556659",
+    "url": "/static/css/main.47752e92.chunk.css"
+  },
+  {
+    "revision": "3a790dea0b146048efd6b2139bd05e96",
+    "url": "/index.html"
+  }
+];
