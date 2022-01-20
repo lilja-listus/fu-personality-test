497c6f5a3b7712bdea1af022161e51c26d664ee7
Lilja Listus <lilja.listus@gmail..com>
2022-01-20 15:59:06 +0100

    update styles

--- src/components/test-button/index.tsx
+++ src/components/test-button/index.tsx
@@ -1,25 +1,67 @@
 import React, { Component } from "react";
 import "./index.scss";
-import Button from "@material-ui/core/Button";
+import ButtonUnstyled, {
+  buttonUnstyledClasses,
+  ButtonUnstyledProps,
+} from '@mui/base/ButtonUnstyled';
+import { styled } from '@mui/system';
+
+
+const blue = {
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0059B2',
+};
+
+const CustomButtonRoot = styled('button')`
+  font-family: IBM Plex Sans, sans-serif;
+  font-weight: bold;
+  font-size: 0.875rem;
+  background-color: ${blue[500]};
+  padding: 12px 24px;
+  border-radius: 8px;
+  color: white;
+  transition: all 150ms ease;
+  cursor: pointer;
+  border: none;
+
+  &:hover {
+    background-color: ${blue[600]};
+  }
+
+  &.${buttonUnstyledClasses.active} {
+    background-color: ${blue[700]};
+  }
+
+  &.${buttonUnstyledClasses.focusVisible} {
+    box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 5px rgba(0, 127, 255, 0.5);
+    outline: none;
+  }
+
+  &.${buttonUnstyledClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+function CustomButton(props: ButtonUnstyledProps) {
+  return <ButtonUnstyled {...props} component={CustomButtonRoot} />;
+}
 
 class QuestionButton extends Component<any, any> {
   handleClick() {
     this.props.clickHandler(this.props.button_text);
     this.props.selectAnswer(this.props.answer);
   }
-
   render() {
     return (
       <div>
-        {" "}
         <div className="button">
-          <Button
-            variant="contained"
-            color="primary"
+          <CustomButton
             onClick={this.handleClick.bind(this)}
           >
-            {this.props.button_text}{" "}
-          </Button>
+            {this.props.button_text}
+          </CustomButton>
         </div>
       </div>
     );
